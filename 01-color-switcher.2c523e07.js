dbf94486feaf839ff4866676416a99464f92ec07
AlinaS13 <117362222+AlinaS13@users.noreply.github.com>
2023-02-01 20:59:18 +0000

    Deploying to gh-pages from @ AlinaS13/goit-js-hw-09@ef42017e30a10994c340c299aac8ac2a5a31738a 🚀

--- 01-color-switcher.2c523e07.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.2c523e07.js.map
