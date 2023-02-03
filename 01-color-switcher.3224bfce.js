b86c11d42bda345af67fee46e260cdc30376b18e
AlinaS13 <117362222+AlinaS13@users.noreply.github.com>
2023-02-03 23:12:06 +0000

    Deploying to gh-pages from @ AlinaS13/goit-js-hw-09@05fc8585882d2b9f80cbfcd077b058405c24807f 🚀

--- 01-color-switcher.3224bfce.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{t.disabled=!0,a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.background=t}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(a)}));
-//# sourceMappingURL=01-color-switcher.3224bfce.js.map
